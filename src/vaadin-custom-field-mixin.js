1ffa8c5da9a54d2754d0371b6de977256e4bf1b7
web-padawan <iamkulykov@gmail.com>
2020-11-05 11:11:13 +0200

    Update copyright headers

--- src/vaadin-custom-field-mixin.js
+++ src/vaadin-custom-field-mixin.js
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2020 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
 import { FlattenedNodesObserver } from '@polymer/polymer/lib/utils/flattened-nodes-observer.js';
 
 /**
